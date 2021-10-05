5142a84027e18dd360f17d23452575fc467c5b60
nilsj <njo@kvalitetsit.dk>
2021-10-05 13:04:04 +0200

    Run setup.sh

--- documentation/docker/getOldDoc.sh
+++ documentation/docker/getOldDoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if docker pull kvalitetsit/kithugs-documentation:latest; then
+if docker pull kvalitetsit/rim-medarbejder-bff-documentation:latest; then
     echo "Copy from old documentation image."
-    docker cp $(docker create kvalitetsit/kithugs-documentation:latest):/usr/share/nginx/html target/old
+    docker cp $(docker create kvalitetsit/rim-medarbejder-bff-documentation:latest):/usr/share/nginx/html target/old
 fi
