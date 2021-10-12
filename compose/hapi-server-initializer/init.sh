f1dd805790661e82bd57413536e0a2e5a165d206
nilsj <njo@kvalitetsit.dk>
2021-10-12 16:13:37 +0200

    Add hapi-server and initialize it.

---
+++ compose/hapi-server-initializer/init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo 'Installing curl ...';
+apk add curl;
+
+echo 'Waiting for hapi-server to be ready ...';
+curl --retry 5 --retry-max-time 40 --retry-connrefused http://hapi-server:8080
+
+echo 'Initializing hapi-server ...';
+curl -i -d '@/hapi-server-initializer/patient1.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/Patient?_format=xml
