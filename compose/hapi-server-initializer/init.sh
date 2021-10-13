f8becfb6ca0027c7e84bd9339a6abf746503f4f7
nilsj <njo@kvalitetsit.dk>
2021-10-13 10:21:27 +0200

    Extend test patient.

--- compose/hapi-server-initializer/init.sh
+++ compose/hapi-server-initializer/init.sh
@@ -4,7 +4,7 @@ echo 'Installing curl ...';
 apk add curl;
 
 echo 'Waiting for hapi-server to be ready ...';
-curl --retry 5 --retry-max-time 40 --retry-connrefused http://hapi-server:8080
+curl -o /dev/null --retry 5 --retry-max-time 40 --retry-connrefused http://hapi-server:8080
 
 echo 'Initializing hapi-server ...';
 curl -i -d '@/hapi-server-initializer/patient1.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/Patient?_format=xml
