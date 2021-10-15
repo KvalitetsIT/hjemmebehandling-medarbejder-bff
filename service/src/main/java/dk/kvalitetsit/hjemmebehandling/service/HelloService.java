23a4f7a27282e7fde7cfe03f6f0c148c2ebe15b8
nilsj <njo@kvalitetsit.dk>
2021-10-15 10:08:48 +0200

    Delete Hello-service.

--- service/src/main/java/dk/kvalitetsit/hjemmebehandling/service/HelloService.java
+++
@@ -1,8 +0,0 @@
-package dk.kvalitetsit.hjemmebehandling.service;
-
-import dk.kvalitetsit.hjemmebehandling.service.model.HelloServiceOutput;
-import dk.kvalitetsit.hjemmebehandling.service.model.HelloServiceInput;
-
-public interface HelloService {
-    HelloServiceOutput helloServiceBusinessLogic(HelloServiceInput input);
-}
