e68134ad5496ff5310648154cd566463c7e95081
nilsj <njo@kvalitetsit.dk>
2021-10-21 16:25:21 +0200

    Yet another attempt ...

---
+++ integrationtest/src/test/resources/compose/hapi-server-initializer/init.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo 'Installing curl ...';
+apk add curl;
+
+echo 'Waiting for hapi-server to be ready ...';
+curl -o /dev/null --retry 5 --retry-max-time 40 --retry-connrefused http://hapi-server:8080
+
+echo 'Initializing hapi-server ...';
+
+# Using PUT allows us to control the resource id's.
+curl -i -X PUT -d '@/hapi-server-initializer/patient-1.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/Patient/patient-1?_format=xml
+
+curl -i -X PUT -d '@/hapi-server-initializer/questionnaire-1.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/Questionnaire/questionnaire-1?_format=xml
+curl -i -X PUT -d '@/hapi-server-initializer/questionnaire-2.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/Questionnaire/questionnaire-2?_format=xml
+
+curl -i -X PUT -d '@/hapi-server-initializer/careplan-1.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/CarePlan/careplan-1?_format=xml
+
+curl -i -X PUT -d '@/hapi-server-initializer/plandefinition-1.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/PlanDefinition/plandefinition-1?_format=xml
+
+echo 'Done initializing hapi-server!';
