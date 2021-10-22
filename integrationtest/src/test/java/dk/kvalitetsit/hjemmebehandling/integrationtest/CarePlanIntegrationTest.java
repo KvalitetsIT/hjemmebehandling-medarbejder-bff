59ecb92adb6b534c371bad5b8a42aa87df8c3f40
nilsj <njo@kvalitetsit.dk>
2021-10-22 09:49:57 +0200

    Try with local compose.

--- integrationtest/src/test/java/dk/kvalitetsit/hjemmebehandling/integrationtest/CarePlanIntegrationTest.java
+++ integrationtest/src/test/java/dk/kvalitetsit/hjemmebehandling/integrationtest/CarePlanIntegrationTest.java
@@ -21,6 +21,7 @@ public class CarePlanIntegrationTest {
 
     @ClassRule
     public static DockerComposeContainer environment = new DockerComposeContainer(new File("/src/compose/docker-compose.yml"))
+            .withLocalCompose(true)
             .waitingFor("hapi-server", Wait.forHttp("/fhir/CarePlan/careplan-1").forStatusCode(200));
 
     @BeforeAll
