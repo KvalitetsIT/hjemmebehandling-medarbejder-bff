25d9d84ea2258189a0bcca32e21fd541362ee750
nilsj <njo@kvalitetsit.dk>
2021-11-15 15:04:52 +0100

    Improve init-script: Check response codes, delete before trying to create.

--- compose/hapi-server-initializer/init.sh
+++ compose/hapi-server-initializer/init.sh
@@ -8,21 +8,61 @@ curl -o /dev/null --retry 5 --retry-max-time 40 --retry-connrefused http://hapi-
 
 echo 'Initializing hapi-server ...';
 
-# Using PUT allows us to control the resource id's.
-curl -i -X PUT -d '@/hapi-server-initializer/patient-1.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/Patient/patient-1?_format=xml
-curl -i -X PUT -d '@/hapi-server-initializer/patient-2.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/Patient/patient-2?_format=xml
+function delete {
+  echo 'Deleting '$1' ...'
 
-curl -i -X PUT -d '@/hapi-server-initializer/questionnaire-1.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/Questionnaire/questionnaire-1?_format=xml
-curl -i -X PUT -d '@/hapi-server-initializer/questionnaire-2.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/Questionnaire/questionnaire-2?_format=xml
+  if [ $(curl -s -o /dev/null -w '%{http_code}' -X DELETE 'http://hapi-server:8080/fhir/'$1) -ne '200' ]
+  then
+    echo 'error deleting object, exiting ...'
+    exit 1
+  else
+    echo 'successfully deleted '$1'!'
+  fi
+}
 
-curl -i -X PUT -d '@/hapi-server-initializer/plandefinition-1.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/PlanDefinition/plandefinition-1?_format=xml
+function create {
+  echo 'Creating '$2' ...'
 
-curl -i -X PUT -d '@/hapi-server-initializer/careplan-1.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/CarePlan/careplan-1?_format=xml
+  # Using PUT allows us to control the resource id's.
+  if [ $(curl -s -o /dev/null -w '%{http_code}' -X PUT -d '@/hapi-server-initializer/'$1 -H 'Content-Type: application/fhir+xml' 'http://hapi-server:8080/fhir/'$2'?_format=xml') -ne '201' ]
+  then
+    echo 'error creating object, exiting ...'
+    exit 1
+  else
+      echo 'successfully created '$2'!'
+  fi
+}
 
-curl -i -X PUT -d '@/hapi-server-initializer/questionnaireresponse-1.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/QuestionnaireResponse/questionnaireresponse-1?_format=xml
-curl -i -X PUT -d '@/hapi-server-initializer/questionnaireresponse-2.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/QuestionnaireResponse/questionnaireresponse-2?_format=xml
-curl -i -X PUT -d '@/hapi-server-initializer/questionnaireresponse-3.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/QuestionnaireResponse/questionnaireresponse-3?_format=xml
+delete 'SearchParameter/searchparameter-examination-status'
 
-curl -i -X PUT -d '@/hapi-server-initializer/searchparameter-examination-status.xml' -H 'Content-Type: application/fhir+xml' http://hapi-server:8080/fhir/SearchParameter/searchparameter-examination-status?_format=xml
+delete 'QuestionnaireResponse/questionnaireresponse-3'
+delete 'QuestionnaireResponse/questionnaireresponse-2'
+delete 'QuestionnaireResponse/questionnaireresponse-1'
+
+delete 'CarePlan/careplan-1'
+
+delete 'PlanDefinition/plandefinition-1'
+
+delete 'Questionnaire/questionnaire-2'
+delete 'Questionnaire/questionnaire-1'
+
+delete 'Patient/patient-2'
+delete 'Patient/patient-1'
+
+create 'patient-1.xml' 'Patient/patient-1'
+create 'patient-2.xml' 'Patient/patient-2'
+
+create 'questionnaire-1.xml' 'Questionnaire/questionnaire-1'
+create 'questionnaire-2.xml' 'Questionnaire/questionnaire-2'
+
+create 'plandefinition-1.xml' 'PlanDefinition/plandefinition-1'
+
+create 'careplan-1.xml' 'CarePlan/careplan-1'
+
+create 'questionnaireresponse-1.xml' 'QuestionnaireResponse/questionnaireresponse-1'
+create 'questionnaireresponse-2.xml' 'QuestionnaireResponse/questionnaireresponse-2'
+create 'questionnaireresponse-3.xml' 'QuestionnaireResponse/questionnaireresponse-3'
+
+create 'searchparameter-examination-status.xml' 'SearchParameter/searchparameter-examination-status'
 
 echo 'Done initializing hapi-server!';
