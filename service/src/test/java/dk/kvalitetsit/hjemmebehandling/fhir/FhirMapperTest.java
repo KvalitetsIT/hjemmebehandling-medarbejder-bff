b183843fd791b9297fb35acad491bb55f4dba8b2
nilsj <njo@kvalitetsit.dk>
2021-11-16 16:47:47 +0100

    Add more tests.

--- service/src/test/java/dk/kvalitetsit/hjemmebehandling/fhir/FhirMapperTest.java
+++ service/src/test/java/dk/kvalitetsit/hjemmebehandling/fhir/FhirMapperTest.java
@@ -4,22 +4,93 @@ import dk.kvalitetsit.hjemmebehandling.constants.AnswerType;
 import dk.kvalitetsit.hjemmebehandling.constants.ExaminationStatus;
 import dk.kvalitetsit.hjemmebehandling.constants.Systems;
 import dk.kvalitetsit.hjemmebehandling.constants.TriagingCategory;
-import dk.kvalitetsit.hjemmebehandling.model.PatientModel;
-import dk.kvalitetsit.hjemmebehandling.model.QuestionAnswerPairModel;
-import dk.kvalitetsit.hjemmebehandling.model.QuestionnaireResponseModel;
+import dk.kvalitetsit.hjemmebehandling.model.*;
 import dk.kvalitetsit.hjemmebehandling.model.answer.AnswerModel;
 import dk.kvalitetsit.hjemmebehandling.model.question.QuestionModel;
+import dk.kvalitetsit.hjemmebehandling.types.Weekday;
+import dk.kvalitetsit.hjemmebehandling.util.DateProvider;
 import org.hl7.fhir.r4.model.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 public class FhirMapperTest {
-    private FhirMapper subject = new FhirMapper();
+    @InjectMocks
+    private FhirMapper subject;
+
+    @Mock
+    private DateProvider dateProvider;
+
+    @Test
+    public void mapCarePlanModel_mapsSubject() {
+        // Arrange
+        CarePlanModel carePlanModel = buildCarePlanModel();
+
+        // Act
+        CarePlan result = subject.mapCarePlanModel(carePlanModel);
+
+        // Assert
+        assertEquals(result.getSubject().getReference(), carePlanModel.getPatient().getId());
+    }
+
+    @Test
+    public void mapCarePlan_mapsPeriod() {
+        // Arrange
+        CarePlan carePlan = new CarePlan();
+        carePlan.setPeriod(new Period());
+        carePlan.setCreated(Date.from(Instant.parse("2021-10-28T00:00:00Z")));
+        carePlan.getPeriod().setStart(Date.from(Instant.parse("2021-10-28T00:00:00Z")));
+        carePlan.getPeriod().setEnd(Date.from(Instant.parse("2021-10-29T00:00:00Z")));
+
+        // Act
+        CarePlanModel result = subject.mapCarePlan(carePlan);
+
+        // Assert
+        assertEquals(result.getStartDate(), Instant.parse("2021-10-28T00:00:00Z"));
+        assertEquals(result.getEndDate(), Instant.parse("2021-10-29T00:00:00Z"));
+    }
+
+    @Test
+    public void mapPatientModel_mapsCpr() {
+        // Arrange
+        PatientModel patientModel = buildPatientModel();
+
+        // Act
+        Patient result = subject.mapPatientModel(patientModel);
+
+        // Assert
+        assertEquals(1, result.getIdentifier().size());
+        assertEquals(Systems.CPR, result.getIdentifier().get(0).getSystem());
+        assertEquals("0101010101", result.getIdentifier().get(0).getValue());
+    }
+
+    @Test
+    public void mapPatient_mapsCpr() {
+        // Arrange
+        Patient patient = new Patient();
+
+        Identifier identifier = new Identifier();
+        identifier.setSystem(Systems.CPR);
+        identifier.setValue("0101010101");
+        patient.getIdentifier().add(identifier);
+
+
+        // Act
+        PatientModel result = subject.mapPatient(patient);
+
+        // Assert
+        assertEquals("0101010101", result.getCpr());
+    }
 
     @Test
     public void mapQuestionnaireResponseModel_mapsAnswers() {
@@ -75,24 +146,51 @@ public class FhirMapperTest {
         assertEquals(AnswerType.INTEGER, result.getQuestionAnswerPairs().get(1).getAnswer().getAnswerType());
     }
 
-    private QuestionnaireResponse.QuestionnaireResponseItemComponent buildStringItem(String value, String linkId) {
-        return buildItem(new StringType(value), linkId);
+    private CarePlanModel buildCarePlanModel() {
+        CarePlanModel carePlanModel = new CarePlanModel();
+
+        carePlanModel.setId("careplan-1");
+        carePlanModel.setPatient(buildPatientModel());
+        carePlanModel.setQuestionnaires(List.of(buildQuestionnaireWrapperModel()));
+        carePlanModel.setPlanDefinitions(List.of(buildPlanDefinitionModel()));
+
+        return carePlanModel;
     }
 
-    private QuestionnaireResponse.QuestionnaireResponseItemComponent buildIntegerItem(int value, String linkId) {
-        return buildItem(new IntegerType(value), linkId);
+    private ContactDetailsModel buildContactDetailsModel() {
+        ContactDetailsModel contactDetailsModel = new ContactDetailsModel();
+
+        contactDetailsModel.setStreet("Fiskergade");
+
+        return contactDetailsModel;
     }
 
-    private QuestionnaireResponse.QuestionnaireResponseItemComponent buildItem(Type value, String linkId) {
-        var item = new QuestionnaireResponse.QuestionnaireResponseItemComponent();
+    private FrequencyModel buildFrequencyModel() {
+        FrequencyModel frequencyModel = new FrequencyModel();
 
-        var answer = new QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent();
-        answer.setValue(value);
-        item.getAnswer().add(answer);
+        frequencyModel.setWeekday(Weekday.FRI);
 
-        item.setLinkId(linkId);
+        return frequencyModel;
+    }
 
-        return item;
+    private PatientModel buildPatientModel() {
+        PatientModel patientModel = new PatientModel();
+
+        patientModel.setCpr("0101010101");
+        patientModel.setPatientContactDetails(buildContactDetailsModel());
+        patientModel.setPrimaryRelativeContactDetails(buildContactDetailsModel());
+        patientModel.setAdditionalRelativeContactDetails(List.of(buildContactDetailsModel()));
+
+        return patientModel;
+    }
+
+    private PlanDefinitionModel buildPlanDefinitionModel() {
+        PlanDefinitionModel planDefinitionModel = new PlanDefinitionModel();
+
+        planDefinitionModel.setId("plandefinition-1");
+        planDefinitionModel.setQuestionnaires(List.of(buildQuestionnaireWrapperModel()));
+
+        return planDefinitionModel;
     }
 
     private Questionnaire.QuestionnaireItemComponent buildQuestionItem(String linkId) {
@@ -125,5 +223,51 @@ public class FhirMapperTest {
         model.setPatient(patientModel);
 
         return model;
+    }
+
+    private QuestionModel buildQuestionModel() {
+        QuestionModel questionModel = new QuestionModel();
+
+        questionModel.setText("Hvordan har du det?");
+
+        return questionModel;
+    }
+
+    private QuestionnaireModel buildQuestionnaireModel() {
+        QuestionnaireModel questionnaireModel = new QuestionnaireModel();
+
+        questionnaireModel.setId("questionnaire-1");
+        questionnaireModel.setQuestions(List.of(buildQuestionModel()));
+
+        return questionnaireModel;
+    }
+
+    private QuestionnaireWrapperModel buildQuestionnaireWrapperModel() {
+        QuestionnaireWrapperModel questionnaireWrapperModel = new QuestionnaireWrapperModel();
+
+        questionnaireWrapperModel.setQuestionnaire(buildQuestionnaireModel());
+        questionnaireWrapperModel.setFrequency(buildFrequencyModel());
+
+        return questionnaireWrapperModel;
+    }
+
+    private QuestionnaireResponse.QuestionnaireResponseItemComponent buildStringItem(String value, String linkId) {
+        return buildItem(new StringType(value), linkId);
+    }
+
+    private QuestionnaireResponse.QuestionnaireResponseItemComponent buildIntegerItem(int value, String linkId) {
+        return buildItem(new IntegerType(value), linkId);
+    }
+
+    private QuestionnaireResponse.QuestionnaireResponseItemComponent buildItem(Type value, String linkId) {
+        var item = new QuestionnaireResponse.QuestionnaireResponseItemComponent();
+
+        var answer = new QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent();
+        answer.setValue(value);
+        item.getAnswer().add(answer);
+
+        item.setLinkId(linkId);
+
+        return item;
     }
 }
