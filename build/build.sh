fefb487faee809d634b5386f4a8e3754cbd81571
nilsj <njo@kvalitetsit.dk>
2021-11-19 10:55:47 +0100

    Disable integration test for now.

--- build/build.sh
+++ build/build.sh
@@ -5,12 +5,8 @@ docker build -t resources -f ./integrationtest/docker/Dockerfile-resources --no-
 docker run -d --name hjemmebehandling-medarbejder-bff-resources resources
 
 # Build hapi-server resource container and start it
-docker rmi -f hapi-server-resources
 docker build -t hapi-server-resources -f ./compose/Dockerfile-hapi-server-resources --no-cache ./compose
 docker run -d --name hapi-server-resources hapi-server-resources
-
-echo 'Testing docker-compose'
-docker-compose version
 
 # Build inside docker container
 docker run -v /var/run/docker.sock:/var/run/docker.sock  -v $HOME/.docker/config.json:/root/.docker/config.json -v $(pwd):/src -v $HOME/.m2:/root/.m2 --volumes-from hjemmebehandling-medarbejder-bff-resources --volumes-from hapi-server-resources maven:3-jdk-11 /src/build/maven.sh
