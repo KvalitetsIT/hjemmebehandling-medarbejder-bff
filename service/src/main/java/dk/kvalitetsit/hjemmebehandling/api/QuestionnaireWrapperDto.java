643efe4d264c6171b3a4a375fc691835ff5c4bec
Sune Koch Hansen <sune@kvalitetsit.dk>
2021-11-24 15:41:46 +0100

    RIM-134: Modellering af alarmgrænser

--- service/src/main/java/dk/kvalitetsit/hjemmebehandling/api/QuestionnaireWrapperDto.java
+++ service/src/main/java/dk/kvalitetsit/hjemmebehandling/api/QuestionnaireWrapperDto.java
@@ -1,8 +1,16 @@
 package dk.kvalitetsit.hjemmebehandling.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuestionnaireWrapperDto {
     private QuestionnaireDto questionnaire;
     private FrequencyDto frequency;
+    private List<ThresholdDto> thresholds;
+
+    public QuestionnaireWrapperDto() {
+        thresholds = new ArrayList<>();
+    }
 
     public QuestionnaireDto getQuestionnaire() {
         return questionnaire;
@@ -18,5 +26,13 @@ public class QuestionnaireWrapperDto {
 
     public void setFrequency(FrequencyDto frequency) {
         this.frequency = frequency;
+    }
+
+    public List<ThresholdDto> getThresholds() {
+        return thresholds;
+    }
+
+    public void setThresholds(List<ThresholdDto> thresholds) {
+        this.thresholds = thresholds;
     }
 }
