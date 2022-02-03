5702bf9b0ec67bc7dd577c475be2555b2fcceb1a
Sune Koch Hansen <sune@kvalitetsit.dk>
2022-02-03 15:22:12 +0100

    RIM-613: alarmgrænser skal ikke længere være på careplan men kun være på plandefinition

--- service/src/main/java/dk/kvalitetsit/hjemmebehandling/fhir/FhirMapper.java
+++ service/src/main/java/dk/kvalitetsit/hjemmebehandling/fhir/FhirMapper.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import dk.kvalitetsit.hjemmebehandling.constants.EnableWhenOperator;
 import dk.kvalitetsit.hjemmebehandling.constants.PlanDefinitionStatus;
