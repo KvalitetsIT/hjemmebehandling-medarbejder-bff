c18a025de7099462262a739736574aec0aa174db
Mikkel Birch <mib@kvalitetsit.dk>
2022-02-04 07:59:23 +0100

    QuestionnaireIntegrationTest

---
+++ integrationtest/src/test/java/dk/kvalitetsit/hjemmebehandling/integrationtest/QuestionnaireIntegrationTest.java
@@ -0,0 +1,29 @@
+package dk.kvalitetsit.hjemmebehandling.integrationtest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openapitools.client.ApiResponse;
+import org.openapitools.client.api.CarePlanApi;
+import org.openapitools.client.api.QuestionnaireApi;
+import org.openapitools.client.model.*;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuestionnaireIntegrationTest extends AbstractIntegrationTest {
+    private QuestionnaireApi questionnaireApi;
+
+    @BeforeEach
+    public void setup() {
+        questionnaireApi = new QuestionnaireApi();
+        questionnaireApi.getApiClient().setBasePath(enhanceBasePath(questionnaireApi.getApiClient().getBasePath()));
+    }
+
+    @Test
+    public void createCarePlan_success() throws Exception {
+        var questionnaire = questionnaireApi.getQuestionnaireById("questionnaire-infektionsmedicinsk-1");
+        assertNotNull(questionnaire);
+    }
+
+}
