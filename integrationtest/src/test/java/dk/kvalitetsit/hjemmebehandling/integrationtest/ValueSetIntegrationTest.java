cd8644397e4dd6077e7f45e3cafcb35912580b15
Sune Koch Hansen <sune@kvalitetsit.dk>
2022-02-22 13:28:25 +0100

    integrationstest til målingstyper

---
+++ integrationtest/src/test/java/dk/kvalitetsit/hjemmebehandling/integrationtest/ValueSetIntegrationTest.java
@@ -0,0 +1,33 @@
+package dk.kvalitetsit.hjemmebehandling.integrationtest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openapitools.client.ApiResponse;
+import org.openapitools.client.api.ValueSetApi;
+import org.openapitools.client.model.MeasurementTypeDto;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ValueSetIntegrationTest extends AbstractIntegrationTest {
+    private ValueSetApi subject;
+
+    @BeforeEach
+    public void setup() {
+        subject = new ValueSetApi();
+
+        subject.getApiClient().setBasePath(enhanceBasePath(subject.getApiClient().getBasePath()));
+    }
+
+    @Test
+    public void getMeasurementTypes_success() throws Exception {
+        // Arrange
+
+        // Act
+        ApiResponse<List<MeasurementTypeDto>> response = subject.getMeasurementTypesWithHttpInfo();
+
+        // Assert
+        assertEquals(200, response.getStatusCode());
+    }
+}
