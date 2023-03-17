8114aeaae34a030dd1215b4bfedad84615f484be
orehoj <mads@kvalitetsit.dk>
2023-03-17 14:35:39 +0100

    Fejlbeskeder opdateret

--- service/src/main/java/dk/kvalitetsit/hjemmebehandling/constants/errors/ErrorDetails.java
+++ service/src/main/java/dk/kvalitetsit/hjemmebehandling/constants/errors/ErrorDetails.java
@@ -1,9 +1,9 @@
 package dk.kvalitetsit.hjemmebehandling.constants.errors;
 
 public enum ErrorDetails {
-    CAREPLAN_EXISTS("Aktiv monitoreringsplan eksisterer allerede for det angivne cpr-nummer.", 10),
+    CAREPLAN_EXISTS("Der eksisterer allerede en aktiv monitoreringsplan for det angivne cpr-nummer.", 10),
     CAREPLAN_DOES_NOT_EXIST("Den angivne monitoreringsplan eksisterer ikke.", 11),
-    CAREPLAN_HAS_UNHANDLED_QUESTIONNAIRERESPONSES("Monitoreringsplanen har ubehandlede besvarelser på et eller flere spørgeskemaer.", 11),
+    CAREPLAN_HAS_UNHANDLED_QUESTIONNAIRERESPONSES("Det er ikke muligt at afslutte patientens monitoreringsplan, da der ligger ubehandlede besvarelser, der skal håndteres, på et eller flere spørgeskemaer.", 11),
     CAREPLAN_ALREADY_FULFILLED("Den angivne monitoreringsplan har ikke alarmer.", 12),
     PLAN_DEFINITIONS_MISSING_FOR_CAREPLAN("De angivne patientgrupper eksisterer ikke.", 13),
     QUESTIONNAIRES_MISSING_FOR_CAREPLAN("De angivne spørgeskemaer eksisterer ikke.", 14),
@@ -19,10 +19,10 @@ public enum ErrorDetails {
     QUESTIONS_ID_IS_NOT_UNIQUE("Alle spørgsmål har ikke en unik forkortelse", 23),
     CUSTOMLOGIN_UNKNOWN_ERROR("Der opstod et ukendt problem ved oprettelse af login",24),
     CPRSERVICE_UNKOWN_ERROR("Der opstod et ukendt problem i forsøget på at hente person udfra cpr-nummer",25),
-    CAREPLAN_IS_MISSING_SCHEDULED_QUESTIONNAIRERESPONSES("Monitoreringsplanen har blå alarmer på et eller flere spørgeskemaer.", 26),
-    QUESTIONNAIRE_IS_IN_ACTIVE_USE_BY_CAREPLAN("Spørgeskemaet bruges i patientgruppe på en eller flere aktive monitoreringsplaner.", 27),
-    PLANDEFINITION_IS_IN_ACTIVE_USE_BY_CAREPLAN("Patientgruppen bruges på en eller flere aktive monitoreringsplaner.", 28),
-    PLANDEFINITION_CONTAINS_QUESTIONNAIRE_WITH_MISSING_SCHEDULED_QUESTIONNAIRERESPONSES("Patientgruppen indeholder et eller flere spørgeskemaer der har blå alarmer.", 29),
+    CAREPLAN_IS_MISSING_SCHEDULED_QUESTIONNAIRERESPONSES("Det er ikke muligt at afslutte patientens monitoreringsplan, da der er blå alarmer, der skal håndteres, på et eller flere spørgeskemaer.", 26),
+    QUESTIONNAIRE_IS_IN_ACTIVE_USE_BY_CAREPLAN("Spørgeskemaet er tilknyttet en patientgruppe, hvor der er en eller flere aktive patienter, der først skal afsluttes.", 27),
+    PLANDEFINITION_IS_IN_ACTIVE_USE_BY_CAREPLAN("Patientgruppen har tilknyttet patienter med en eller flere aktive monitoreringsplaner, der først skal afsluttes.", 28),
+    PLANDEFINITION_CONTAINS_QUESTIONNAIRE_WITH_MISSING_SCHEDULED_QUESTIONNAIRERESPONSES("Patientgruppen indeholder et eller flere spørgeskemaer, der har blå alarmer.", 29),
     PLANDEFINITION_CONTAINS_QUESTIONNAIRE_WITH_UNHANDLED_QUESTIONNAIRERESPONSES("Patienggruppen indeholder et eller flere spørgeskemaer der har ubehandlede besvarelser.", 30),
 
 
